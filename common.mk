c59b12793f2e227bee3fdab66ceadb320ddb7b31
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-11-11 10:18:07 +0530

    sm8150-common: Drop touch HAL
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- common.mk
+++ common.mk
@@ -484,10 +484,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oneplus_msmnile
-
 # tri-state key
 PRODUCT_PACKAGES += \
     tri-state-key_daemon
