7414dfb598432deb46800ecd7ce2454b9544b115
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-01-11 15:17:08 +0100

    sm8150-common: drop local telephony-ext bootjar definition
    * We already have it in platform
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- common.mk
+++ common.mk
@@ -466,9 +466,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-ims.xml
 
